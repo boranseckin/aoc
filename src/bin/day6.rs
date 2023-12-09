e1382c139b1ab923476248caaf84452d6ea2d4b8
Boran Seckin <boran@boranseckin.com>
2023-12-09 17:36:20 -0500

    feat: day6

---
+++ src/bin/day6.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let input = include_str!("../../inputs/day6.input");
+
+    let mut input = input
+        .lines()
+        .map(|line| {
+            line.split_once(':')
+                .unwrap()
+                .1
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<usize>>()
+        });
+    let times = input.next().unwrap();
+    let distances = input.next().unwrap();
+
+    let wins: usize = times.into_iter().zip(distances).map(|(time, distance)| {
+        (1..time)
+            .filter(|t| (time - t) * t > distance)
+            .collect::<Vec<usize>>()
+            .len()
+    })
+    .reduce(|acc, e| acc * e)
+    .unwrap();
+
+    dbg!(wins);
+
+
+    let input = include_str!("../../inputs/day6.input");
+
+    let mut input = input
+        .lines()
+        .map(|line| {
+            line.split_once(':')
+                .unwrap()
+                .1
+                .split_whitespace()
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap()
+        });
+    let time = input.next().unwrap();
+    let distance = input.next().unwrap();
+
+    let wins = (1..time)
+        .filter(|t| (time - t) * t > distance)
+        .collect::<Vec<usize>>()
+        .len();
+
+    dbg!(wins);
+}
